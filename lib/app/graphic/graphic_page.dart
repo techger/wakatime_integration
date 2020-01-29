6c8356654ef60c43a4be49cd34ad16c0729e9b3a
Joao Witor Felipe <joaowitorfelipe2@gmail.com>
2020-01-29 20:51:25 -0200

    Adiciona biblioteca para graficos e graficos estaticos para teste

--- lib/app/graphic/graphic_page.dart
+++ lib/app/graphic/graphic_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class GraphicPage extends StatefulWidget {
   final String title;
+
   const GraphicPage({Key key, this.title = "Graphic"}) : super(key: key);
 
   @override
@@ -9,12 +11,65 @@ class GraphicPage extends StatefulWidget {
 }
 
 class _GraphicPageState extends State<GraphicPage> {
+  final List<charts.Series> seriesList = _createSampleData();
+
+  Widget _buildChartPie({test = false}) {
+    return Container(
+      height: 200.0,
+      width: 150.0,
+      child: charts.PieChart(
+        seriesList,
+        animate: true,
+        animationDuration: Duration(milliseconds: 800),
+        // Configure the width of the pie slices to 60px. The remaining space in
+        // the chart will be left as a hole in the center.
+        defaultRenderer: new charts.ArcRendererConfig(
+          arcWidth: 25,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Graphic'),
+        child: Wrap(
+          children: <Widget>[
+            _buildChartPie(test: true),
+            _buildChartPie(),
+            _buildChartPie(),
+            _buildChartPie(),
+          ],
+        ),
       ),
     );
   }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<LinearSales, int>> _createSampleData() {
+    final data = [
+      new LinearSales(0, 100),
+      new LinearSales(1, 75),
+      new LinearSales(2, 25),
+      new LinearSales(3, 5),
+    ];
+
+    return [
+      new charts.Series<LinearSales, int>(
+        id: 'Sales',
+        domainFn: (LinearSales sales, _) => sales.year,
+        measureFn: (LinearSales sales, _) => sales.sales,
+        data: data,
+      )
+    ];
+  }
+}
+
+/// Sample linear data type.
+class LinearSales {
+  final int year;
+  final int sales;
+
+  LinearSales(this.year, this.sales);
 }
